83d279f8062de0355aa3e1305cdb942bb433d91f
Toyfon <90236783+Toyfon@users.noreply.github.com>
2022-02-17 21:51:09 +0300

    fix bug in pagination

--- src/Components/Pagination/Pagination.tsx
+++ src/Components/Pagination/Pagination.tsx
@@ -16,69 +16,66 @@ type Props = {
 }
 
 
-export const Pagination: FC<Props> = React.memo((props) => {
-        const {movieTotalCount, showResultByTitle, value, currentPage, setCurrentPage, movies} = props
+export const Pagination: FC<Props> = (props) => {
+    const {movieTotalCount, showResultByTitle, value, currentPage, setCurrentPage, movies} = props
 
-        const moviesOnPage = 10
-        let pages: number[] = []
+    const moviesOnPage = 10
+    let pages: number[] = []
+    const pagesCount = Math.ceil(movieTotalCount / moviesOnPage);
 
-        const pagesCount = Math.ceil(movieTotalCount / moviesOnPage);
+    if (movieTotalCount && currentPage) {
 
-        if (movieTotalCount && currentPage) {
-            if (currentPage && pagesCount <= 9) {
-                for (let i = 1; i <= pagesCount; i += 1) {
-                    pages.push(i)
-                }
+        if (currentPage && pagesCount <= 9) {
+            for (let i = 1; i <= pagesCount; i += 1) {
+                pages.push(i)
             }
-            if (currentPage >= 6 && currentPage < pagesCount - 6) {
-                for (let i = currentPage - 4; i <= currentPage + 4; i += 1) {
-                    pages.push(i)
-                }
+        } else if (currentPage >= 6 && currentPage < pagesCount - 6) {
+            for (let i = currentPage - 4; i <= currentPage + 4; i += 1) {
+                pages.push(i)
             }
-            if (currentPage <= 6) {
-                pages = Array(9)
-                    .fill(0)
-                    .map((_, i) => i + 1)
-            } else {
-                for (let i = pagesCount - 8; i < pagesCount; i += 1) {
-                    pages.push(i)
-                }
+        } else if (currentPage <= 6) {
+            pages = Array(9)
+                .fill(0)
+                .map((_, i) => i + 1)
+        } else {
+            for (let i = pagesCount - 8; i <= pagesCount; i += 1) {
+                pages.push(i)
             }
         }
+    }
 
-        const onChangeCurrentPage = useCallback((page: number) => {
-            setCurrentPage(page)
-            showResultByTitle(value, page)
-        },[setCurrentPage,showResultByTitle,value])
+    const onChangeCurrentPage = useCallback((page: number) => {
+        setCurrentPage(page)
+        showResultByTitle(value, page)
+    },[setCurrentPage,showResultByTitle,value])
 
-        const mappedPages = pages.map((el) => (
-            <div className={currentPage === el ? s.selectedPage : s.pageNumber}
-                 key={el}
-                 onClick={() => onChangeCurrentPage(el)}
-            >
-                {el}
-            </div>
-        ));
+    const mappedPages = pages.map((el) => (
+        <div className={currentPage === el ? s.selectedPage : s.pageNumber}
+             key={el}
+             onClick={() => onChangeCurrentPage(el)}
+        >
+            {el}
+        </div>
+    ));
 
-        return (<>
-                {movies.length > 0 &&
-                <div className={s.paginator}>
-                    <div className={s.pages}>
-                        {currentPage === 1
-                            ? ''
-                            : <span onClick={() => onChangeCurrentPage(currentPage - 1)}>
+    console.log(currentPage)
+    console.log(pagesCount)
+    return (<>
+            {movies.length > 0 &&
+            <div className={s.paginator}>
+                <div className={s.pages}>
+                    {currentPage !== 1 &&
+                    <span onClick={() => onChangeCurrentPage(currentPage - 1)}>
                     <img src={backArrow} alt="back icon"/>
                 </span>}
-                        {mappedPages}
-                        {currentPage === Math.ceil(movieTotalCount / moviesOnPage)
-                            ? ''
-                            : <span onClick={() => onChangeCurrentPage(currentPage + 1)}>
+                    {mappedPages}
+                    {currentPage !== pagesCount &&
+                    <span onClick={() => onChangeCurrentPage(currentPage + 1)}>
                         <img src={forwardArrow} alt="forward icon"/>
                     </span>}
-                    </div>
                 </div>
-                }
-            </>
-        )
-    }
-)
+            </div>
+            }
+        </>
+    )
+}
